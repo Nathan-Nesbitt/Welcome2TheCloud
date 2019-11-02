983138e8d1d98cb2fe70299e3eae24d2e4c9f8f1
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-02 01:25:51 -0700

    Testing AddCart tests

--- tests/AddCartTest.php
+++ tests/AddCartTest.php
@@ -3,7 +3,9 @@ use PHPUnit\Framework\TestCase;
 
 final class AddCartTest extends TestCase
 {
-    
+    function GetCurrentItemsTest() {
+        $this -> assertNotNull(getCurrentItems());
+    }
 }
 
 ?>
