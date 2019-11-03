ca689e4e5a48cb3b28948f0c7ae9604a10a7eda7
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-02 18:14:13 -0700

    Test for git push script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 set -e
 ssh git@connect.welcome2the.cloud -v exit
-
-git config --global push.default simple # we only want to push one branch — master
-# specify the repo on the live server as a remote repo, and name it 'production'
-# <user> here is the separate user you created for deploying
-git remote add production ssh://git@connect.welcome2the.cloud/Welcome2TheCloud/
-git push production master # push our updates
