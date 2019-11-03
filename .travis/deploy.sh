fbba98e2ea361b5eba4e27bb2a327579d882947d
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-02 19:22:14 -0700

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,10 +4,10 @@
 ssh-keyscan -H connect.welcome2the.cloud >> ~/.ssh/known_hosts
 
 set -e
-ssh -i ~/.ssh/deploy_rsa git@connect.welcome2the.cloud -v exit
+# ssh -i ~/.ssh/deploy_rsa git@connect.welcome2the.cloud -v exit # Testing to see if we can connect
 #
-#git config --global push.default simple # we only want to push one branch — master
+git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-#git remote add production ssh://git@connect.welcome2the.cloud/Welcome2TheCloud/
-#git push production master # push our updates
+git remote add production ssh://git@connect.welcome2the.cloud/Welcome2TheCloud/
+git push production master # push our updates
