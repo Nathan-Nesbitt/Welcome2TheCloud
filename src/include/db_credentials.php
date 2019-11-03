a12393b21f112799258b28e0d8ba3f2cb00a394c
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-03 12:48:04 -0800

    Delete db_credentials.php
    Trying to fix push issues

--- src/include/db_credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$username = "";
-	$password = "";
-	$database = "";
-	$server = "localhost";
-	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
-?>
