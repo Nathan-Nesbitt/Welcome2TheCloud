0504ecaf292f1f2d16e0b41696630e92f2ec453e
Nathan <Nathan-Nesbitt@users.noreply.github.com>
2019-11-07 23:59:55 -0800

    Create db_credentials.php
    Created the db_credentials file now that I know it won't get pushed.

---
+++ src/include/db_credentials.php
@@ -0,0 +1,7 @@
+<?php
+	$username = "";
+	$password = "";
+	$database = "";
+	$server = "localhost";
+	$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
+?>
