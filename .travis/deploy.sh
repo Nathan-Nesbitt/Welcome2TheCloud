f40071a74945ee506a1e059e6643b2777dfa4c1c
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-07 22:17:21 -0800

    Fix for error

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,4 +9,4 @@ set -e
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/Welcome2TheCloud/ git@connect.welcome2the.cloud:/home/git/Welcome2TheCloud/ # This rsyncs the files 
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/ git@connect.welcome2the.cloud:/home/git/Welcome2TheCloud/ # This rsyncs the files 
