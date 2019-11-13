f43aba5bbb214162b2441dfc3dc5f31b7325252e
evgo-space <55894072+evgo-space@users.noreply.github.com>
2019-11-12 16:44:04 -0800

    work on listprod
    links working formating needs work.

--- src/listprod.php
+++ src/listprod.php
@@ -14,27 +14,76 @@
 </form>
 
 <?php
-	include 'include/db_credentials.php';
 
+	
+	include 'include/db_credentials.php';
+	
 	/** Get product name to search for **/
 	if (isset($_GET['productName'])){
 		$name = $_GET['productName'];
 	}
-
+	
+		
 	/** $name now contains the search string the user entered
 	 Use it to build a query and print out the results. **/
-
-	/** Create and validate connection **/
-
+	 /** Create and validate connection **/
+	 //connecting to database
+	 $mysqli = new mysqli($localhost, $username, $password, $database);
+	 if ($mysqli->connect_errno) {
+		 echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	 }
+	 //echo $mysqli->host_info . "\n";
+	 if($name == "all"){
+		 $sql = "SELECT * FROM product ";
+	 }
+	 else{
+	 $sql = "SELECT * FROM product WHERE productname LIKE "  . "'%$name%'" ;
+	 }
+	 $result = mysqli_query($mysqli, $sql);
+	 
+	 if ($result->num_rows > 0) {
 	/** Print out the ResultSet **/
-
+	// output data of each row
 	/** 
 	For each product create a link of the form
 	addcart.php?id=<productId>&name=<productName>&price=<productPrice>
 	Note: As some product names contain special characters, you may need to encode URL parameter for product name like this: urlencode($productName)
 	**/
+    	while($row = $result->fetch_assoc()) {
+			echo "<tr>
+			productId: " . $row["productId"]. " " .
+			 "productName: " . $row["productName"]. " " .
+			  "Price: " . $row["productPrice"] . " " .
+			   "Description: " . $row["productDesc"] . "</td>
+			    <td><a href='addcart.php?id= ".$row["productId"]."&name=".urlencode($row["productName"]) . " &price=".number_format($row["productPrice"],2)."</a></td><br>";
+			   	 
+    	}
+	} else {
+		echo "0 results";
+		
+
 	
-	/** Close connection **/
+
+
+}
+/** Close connection **/
+$conn->close();
+
+
+
+
+
+	
+   
+	
+	
+	
+
+	
+
+	
+	
+	
 
 	/**
         Useful code for formatting currency:
