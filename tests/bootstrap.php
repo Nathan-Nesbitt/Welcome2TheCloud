d5441caad815b82d3fad3f1c1559e65e83b515ce
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-12 18:57:56 -0800

    Changes to PHPunit and Composer

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php 
+    require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../src/addcart.php';
+    require_once __DIR__ . '/../src/checkout.php';
+    require_once __DIR__ . '/../src/listOrder.php';
+    require_once __DIR__ . '/../src/listProd.php';
+    require_once __DIR__ . '/../src/loadData.php';
+    require_once __DIR__ . '/../src/order.php';
+    require_once __DIR__ . '/../src/showcart.php';
+?>
