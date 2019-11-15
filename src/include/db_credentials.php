62d134648045d21b7a67ab588c6735b4f9aec6d6
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-14 18:18:27 -0800

    Removed Database Credentials

--- src/include/db_credentials.php
+++ src/include/db_credentials.php
@@ -1,10 +1,4 @@
 <?php
-	//$username = "evanerson@localhost";
-	//$password = "eaoe51";
-	//$database = "welcome2thecloud";
-	//$server = "localhost";
-	//$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
-
 	function getDBLogin(){
 		$username = "";
 		$password = "";
