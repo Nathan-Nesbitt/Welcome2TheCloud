4c6d3e1e6caa0406232a5d07a1fdaaf2e90e611a
evgo-space <55894072+evgo-space@users.noreply.github.com>
2019-11-15 15:58:27 -0800

    Update db_credentials.php

--- src/include/db_credentials.php
+++ src/include/db_credentials.php
@@ -6,10 +6,7 @@
 	//$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password, "CharacterSet" => "UTF-8");
 
 	function getDBLogin(){
-		$username = "";
-		$password = "";
-		$database = "";
-		$server = "";
+		
 
 		return array($username, $password, $database, $server);
 	}
