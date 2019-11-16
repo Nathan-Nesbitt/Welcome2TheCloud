77866ad6c80ec6a93c6b065993b1a6fd8ce59a4f
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-15 17:10:51 -0800

    Update login.php
    Moved login functions to login_scripts.php to allow for function sharing.
    
    This now sets a token in the browser which has the login hash for the current user. This will allow us to track and verify users accross pages.

--- src/login.php
+++ src/login.php
@@ -1,37 +1,7 @@
 <?php
 
     require_once 'include/db_connection.php';
-
-    function getUserValues() {
-        if (isset($_GET['userid'])){
-            $userid = $_GET['userid'];
-        }
-        if (isset($_GET['password'])){
-            $password = $_GET['password'];
-        }
-        return array($userid, $password);
-    }
-
-    function login($connection, $userid, $password) {
-        $query = $connection->prepare("SELECT password FROM customer WHERE userid = ?");
-        $query->bind_param("s", $userid); 
-        $query->execute();
-        $serverPassword = $query->get_result()->fetch_assoc();
-
-        /*  
-            ONCE THE HASH IS SET UP THIS IS WHERE THE 
-            HASH FUNCTION WOULD GO, CONVERTING PASSWORD FROM
-            USER TO HASH FOR SERVER USING SALT IN TABLE. 
-        */
-
-        if ($password == $serverPassword["password"]) {
-            return TRUE;
-        }
-        else {
-            return FALSE;
-        }
-
-    }
+    require_once 'login_scripts.php';
 
     /* Creates a connection */
     $connection = createConnection();
@@ -40,8 +10,12 @@
     /* Checks to see if the user and pass are correct */
     $loginResult = login($connection, $userid, $password);
     
-    if($loginResult)
+    if($loginResult) {
         echo "SUCCESS LOGGED IN";
+        /* Creates a random token to auth the user for the session */
+        createToken($connection, $userid);
+
+    }
     else
         echo "FAILED, WRONG PASSWORD";
     
