e62027e9a09f7589d81811fcac98efbe1ae45477
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-18 14:07:35 -0800

    Update createAccount.php
    Changed to POST requests and auto-logs in the user on success

--- src/createAccount.php
+++ src/createAccount.php
@@ -1,41 +1,43 @@
 <?php
 require_once 'include/db_connection.php';
+require_once 'login_scripts.php';
+
 
 /* Function to get the values from the form */
 function getUserValues() {
 
-	if (isset($_GET['FirstName'])){
-		$fname = $_GET['FirstName'];
+	if (isset($_POST['FirstName'])){
+		$fname = $_POST['FirstName'];
 	}
-	if (isset($_GET['LastName'])){
-		$lname = $_GET['LastName'];
+	if (isset($_POST['LastName'])){
+		$lname = $_POST['LastName'];
 	}
-	if (isset($_GET['email'])){
-		$email = $_GET['email'];
+	if (isset($_POST['email'])){
+		$email = $_POST['email'];
 	}
-	if (isset($_GET['phonenum'])){
-		$phonenum = $_GET['phonenum'];
+	if (isset($_POST['phonenum'])){
+		$phonenum = $_POST['phonenum'];
 	}
-	if (isset($_GET['address'])){
-		$address = $_GET['address'];
+	if (isset($_POST['address'])){
+		$address = $_POST['address'];
 	}
-	if (isset($_GET['city'])){
-		$city = $_GET['city'];
+	if (isset($_POST['city'])){
+		$city = $_POST['city'];
 	}
-	if (isset($_GET['state'])){
-		$state = $_GET['state'];
+	if (isset($_POST['state'])){
+		$state = $_POST['state'];
 	}
-	if (isset($_GET['postalCode'])){
-		$postalCode = $_GET['postalCode'];
+	if (isset($_POST['postalCode'])){
+		$postalCode = $_POST['postalCode'];
 	}
-	if (isset($_GET['country'])){
-		$country = $_GET['country'];
+	if (isset($_POST['country'])){
+		$country = $_POST['country'];
 	}
-	if (isset($_GET['userid'])){
-		$userid = $_GET['userid'];
+	if (isset($_POST['userid'])){
+		$userid = $_POST['userid'];
 	}
-	if (isset($_GET['password'])){
-		$password = $_GET['password'];
+	if (isset($_POST['password'])){
+		$password = $_POST['password'];
 	}
 	
 	return array($fname, $lname, $email, $phonenum, $address, $city, $state, $postalCode, $country, $userid, $password);
@@ -59,19 +61,23 @@ function createAccount($connection) {
 	/* Passes the values into the query */
 	$query->bind_param("sssssssssss", $fname, $lname, $email, $phonenum, $address, $city, $state, $postalCode, $country, $userid, $password);
 	
-	$query->execute();
+	$result = $query->execute();
 
 	/* Returns TRUE if successful, and FALSE if failed */
-	$result = $query->get_result();
-	return $result;
+	return array($result, $userid);
 }
 
 /* Main function for this page, prints out all orders and all products */
 function mainCreateFunction() {
 		/* Creates the connection to the database */
 		$connection = createConnection();
-		$result = createAccount($connection);
+		list($success, $userid) = createAccount($connection);
+		// If the account is successfully created, log the account in
+		if ($success)
+			createToken($connection, $userid);
+
 	}
 
 mainCreateFunction();
+
 ?>
