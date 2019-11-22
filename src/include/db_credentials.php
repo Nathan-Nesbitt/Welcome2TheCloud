25850cc15d9fd96948ce0263d93adb9d500bc547
evgo-space <55894072+evgo-space@users.noreply.github.com>
2019-11-21 16:49:27 -0800

    product page now has images and link back to search
    NOTE:  if the query for the productImageURL returns null the program will run the alt_getImage fucntion which will retrieve the data from productImage, the code to
    display said data can go into the displayImage function...

--- src/include/db_credentials.php
+++ src/include/db_credentials.php
@@ -1,21 +1,12 @@
 <?php
 	function getDBLogin(){
-<<<<<<< HEAD
 
 		$username = "";
 		$password = "";
 		$database = "";
 		$server = "";
-		
-=======
-		$username = "";
-		$password = "";
-		$database = "";
-		$server = "";
->>>>>>> 4a58fdcc23bf8e85e439bdc73b68f76a5dc62c07
 
 		return array($username, $password, $database, $server);
 	}
 
 ?>
-s
