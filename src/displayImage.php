70dda1a1880703fde903ff076490b9a30dfaeee8
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-22 14:00:19 -0800

    Fixed functions for displayImage

--- src/displayImage.php
+++ src/displayImage.php
@@ -10,25 +10,22 @@ function getId(){
 
 function alt_getImage($connection){
     $s = getId();
-    $s = 11;
 
     $query = $connection->prepare("SELECT productImage FROM product WHERE productId = ?");
     $query->bind_param("i", $s);
     $query->execute();
     $result = $query->get_result();
     return $result;
-
-    $result = alt_getImage($connection);
-            
-            $row = $result->fetch_assoc();
-            echo "".base64_encode($row["productImage"]); 
-            $connection->close();
-
 }
 
 function createImage($connection, $id){
-    if ($id != null)
-        alt_getImage($connection);
+    if ($id != null){
+        $result = alt_getImage($connection);
+            
+        $row = $result->fetch_assoc();
+        echo $row["productImage"]; 
+        $connection->close();
+    }
 }
 
 $id = $_GET['id'];
