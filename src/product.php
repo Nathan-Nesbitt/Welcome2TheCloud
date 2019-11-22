5d2a2530bc025044113d4a012dc330e690e4cf75
evgo-space <55894072+evgo-space@users.noreply.github.com>
2019-11-22 15:30:02 -0800

    nav bar and scaling fixed

--- src/product.php
+++ src/product.php
@@ -106,36 +106,50 @@ function displayImage(){
 
 ?>
 
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html>
 <head>
 <title>w2tdotc - Product Information</title>
+        <link rel="icon" type="image/png" href="images/Welcome2TheCloud.png" type="image/x-icon">
         <link rel="stylesheet" href="shop.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="icon" type="image/png" href="images/Welcome2TheCloud.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+	</script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+	</script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+		integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+	</script>
 </head>
 <body>
-        <nav class="navbar sticky-top navbar-expand-lg navbar-light">
-                <img alt="Brand" src="images/Welcome2TheCloud.png" style="width: 50px">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">        
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                                <li class="nav-item">
-                                        <a class="nav-link" href="/">Homepage<span class="sr-only"></span></a>
-                                </li>
-                                <li class="nav-item">
-                                        <a class="nav-link" href="listprod.php">Products</a>
-                                </li>
-                                <li class="nav-item">
-                                        <a class="nav-link" href="listorder.php">Orders</a>
-                                </li>
-                                <li class="nav-item">
-                                        <a class="nav-link" href="createAccount.html">Create Account</a>
-                                </li>
-                        </ul>
-                </div>
-        </nav>               
+<nav class="navbar sticky-top navbar-expand-lg navbar-light">
+		<img alt="Brand" src="images/Welcome2TheCloud.png" style="width: 50px">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+			aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul id="navbar-ul" class="navbar-nav">
+				<li class="nav-item">
+					<a class="nav-link" href="/">Homepage<span class="sr-only"></span></a>
+				</li>
+				<li class="nav-item active">
+					<a class="nav-link" href="listprod.php">Products</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="listorder.php">Orders</a>
+				</li>
+				<li class="nav-item">
+					<a id="login-nav" class="nav-link" href="login.html">Login</a>
+				</li>
+			</ul>
+		</div>
+	</nav>                 
 </body>
 </html>
 </html>
