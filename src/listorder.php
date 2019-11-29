acd1f3d61039ca63904bbb6be88813709ffdfd94
Nathan Nesbitt <nathan@nesbitt.ca>
2019-11-29 15:45:53 -0800

    Fixed Navbar for dropdown

--- src/listorder.php
+++ src/listorder.php
@@ -147,13 +147,13 @@ function printTable($connection) {
                     loginElement.href = 'customer.php';
                     loginElement.innerHTML = cookieExists[0];
 
-                    // Add Admin Navbar Button //
-                    newLi = '<li class="nav-item"><a class="nav-link" href="admin.php">Admin</a></li>';
-                    $("#navbar-ul").append(newLi);
-                    
-                    // Add the add product navbar button //
-                    newLi = '<li class="nav-item"><a class="nav-link" href="addProduct.html">Add a Product</a></li>';
-                    $("#navbar-ul").append(newLi);
+                    newLi = '<li class="nav-item dropdown">';
+					newLi += '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Admin</a>';
+					newLi += '<div class="dropdown-menu" aria-labelledby="navbarDropdown">';
+					newLi += '<a class="dropdown-item" href="/admin.php">Admin Overview</a>';
+					newLi += '<a class="dropdown-item" href="/addProduct.html">Add Product</a>';
+					newLi += '</li>';
+					$("#navbar-ul").append(newLi);
 
                     // Add the logout navbar button //
                     newLi = '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
