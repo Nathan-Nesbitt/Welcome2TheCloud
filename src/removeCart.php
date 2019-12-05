9fb4db9ca473476762d9868cfa29f96d090a6d9e
Nathan Nesbitt <nathan@nesbitt.ca>
2019-12-05 15:16:01 -0800

    Moved Functions out of Files
    Functions should not be defined in called php files, as it causes redudancy.

--- src/removeCart.php
+++ src/removeCart.php
@@ -1,48 +1,5 @@
 <?php
 
-function getCurrentItems() {
-	// Get the current list of products
-	if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
-	$productList = null;
-	if (isset($_SESSION['productList'])){
-		$productList = $_SESSION['productList'];
-	} else{ 	// No products currently in list.  Create a list.
-		$productList = array();
-	}
-	
-	return $productList;
-};
-
-function removeItemFromCart() {
-    session_start();
-    $productList = getCurrentItems();
-    foreach ($productList as $key => $value) {
-        echo "<br/>";
-        echo "Key: " . $key;
-    }
-    echo "<br/>";
-    var_dump($productList[" 6"]);
-    echo "<br/>";
-    echo $_GET['id'];
-    echo "<br/>";
-
-    if (isset($_GET['id'])) {
-        $id = " ".$_GET['id'];
-    } else {
-        header('Location: listprod.php');
-    }
-
-    unset($productList[$id]);
-
-    $_SESSION['productList'] = $productList;
-
-	header('Location: showcart.php');
-
-};
-
+require_once 'shoppingCart_scripts.php';
 removeItemFromCart();
-
 ?>
---
+++ src/shoppingCart_scripts.php
@@ -0,0 +1,82 @@
+<?php
+
+function getCurrentItems() {
+	/**
+	 * Get the current list of products. 
+	 * 
+	 * Returns: Product List Array  
+	 **/
+
+	if(!isset($_SESSION)) { 
+        session_start(); 
+	}
+	
+	$productList = null;
+	if (isset($_SESSION['productList'])) {
+		$productList = $_SESSION['productList'];
+	}
+	else{
+		// No products currently in list. Create a list.
+		$productList = array();
+	}
+	
+	return $productList;
+};
+
+function addItemToCart() {
+	/**
+	 * Adds the items in the product list to the cart. 
+	 * 
+	 * Returns: 
+	 **/
+
+	$productList = getCurrentItems();
+
+	// Add new product selected
+	// Get product information
+	if(isset($_GET['id']) && isset($_GET['name']) && isset($_GET['price'])){
+		$id = $_GET['id'];
+		$name = $_GET['name'];
+		$price = $_GET['price'];
+	} else {
+		header('Location: listprod.php');
+	}
+
+	// Update quantity if add same item to order again
+	if (isset($productList[$id])){
+		$productList[$id]['quantity'] = $productList[$id]['quantity'] + 1;
+	} else {
+		$productList[$id] = array( "id"=>$id, "name"=>$name, "price"=>$price, "quantity"=>1 );
+	}
+
+	$_SESSION['productList'] = $productList;
+	header('Location: showcart.php');
+}
+
+function removeItemFromCart() {
+    session_start();
+    $productList = getCurrentItems();
+    foreach ($productList as $key => $value) {
+        echo "<br/>";
+        echo "Key: " . $key;
+    }
+    echo "<br/>";
+    var_dump($productList[" 6"]);
+    echo "<br/>";
+    echo $_GET['id'];
+    echo "<br/>";
+
+    if (isset($_GET['id'])) {
+        $id = " ".$_GET['id'];
+    } else {
+        header('Location: listprod.php');
+    }
+
+    unset($productList[$id]);
+
+    $_SESSION['productList'] = $productList;
+
+	header('Location: showcart.php');
+
+};
+?>
