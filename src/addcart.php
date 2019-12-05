0762c6b3cdb07397aaa9e2ba1f656589ef4efb06
Nathan Nesbitt <nathan@nesbitt.ca>
2019-12-05 15:18:30 -0800

    Removed Repeat-Defined Functions
    These functions were already defined in shoppingCart_scripts.php

--- src/addcart.php
+++ src/addcart.php
@@ -1,44 +1,6 @@
 <?php
 
-function getCurrentItems() {
-	// Get the current list of products
-	if(!isset($_SESSION)) 
-    { 
-        session_start(); 
-    } 
-	$productList = null;
-	if (isset($_SESSION['productList'])){
-		$productList = $_SESSION['productList'];
-	} else{ 	// No products currently in list.  Create a list.
-		$productList = array();
-	}
-	
-	return $productList;
-};
-
-function addItemToCart() {
-
-	$productList = getCurrentItems();
-
-	// Add new product selected
-	// Get product information
-	if(isset($_GET['id']) && isset($_GET['name']) && isset($_GET['price'])){
-		$id = $_GET['id'];
-		$name = $_GET['name'];
-		$price = $_GET['price'];
-	} else {
-		header('Location: listprod.php');
-	}
-
-	// Update quantity if add same item to order again
-	if (isset($productList[$id])){
-		$productList[$id]['quantity'] = $productList[$id]['quantity'] + 1;
-	} else {
-		$productList[$id] = array( "id"=>$id, "name"=>$name, "price"=>$price, "quantity"=>1 );
-	}
-
-	$_SESSION['productList'] = $productList;
-	header('Location: showcart.php');
-}
+require_once 'shoppingCart_scripts.php';
 addItemToCart();
+
 ?>
