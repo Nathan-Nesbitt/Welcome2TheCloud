268e43412a1f9b1f4784c51484a02e1392b88339
Nathan Nesbitt <nathan@nesbitt.ca>
2019-12-06 10:33:58 -0800

    Moved Function to Showcart
    This function (showcart) was printing to the screen, which isn't a general function.

--- src/showcart.php
+++ src/showcart.php
@@ -1,5 +1,40 @@
 <?php
 require_once 'shoppingCart_scripts.php';
+// Get the current list of products
+function showCart() {
+	session_start();
+	$productList = null;
+	if (isset($_SESSION['productList'])){
+		$productList = $_SESSION['productList'];
+		echo("<div align='center'><h1>Shopping Cart</h1></div>");
+		echo("<div class='row justify-content-md-center'><div class='col-sm-8'><table class='table'><tr><th class='d-none d-sm-block'>Id</th><th>Product Name</th><th>Quantity</th>");
+		echo("<th class='d-none d-sm-block'>Price</th><th>Subtotal</th><th>Remove</th><th>Update</th></tr>");
+
+		$total =0;
+		foreach ($productList as $id => $prod) {
+			echo("<tr><td class='d-none d-sm-block'>". $prod['id'] . "</td>");
+			echo("<td>" . $prod['name'] . "</td>");
+
+			echo('<form method="post" action="updateQuantity.php?id=' . substr($id, 1) . '">');
+			echo('<td align="center"><input class = "form-control" name="new_quantity" type="number" min="1" value="' .$prod['quantity'] . '">');
+			//echo("<td align=\"center\">". $prod['quantity'] . "</td>");
+			$price = $prod['price'];
+
+			echo("<td class='d-none d-sm-block' align=\"right\">$" . number_format($price ,2) ."</td>");
+			echo("<td align=\"right\">$" . number_format($prod['quantity']*$price, 2) . "</td>");
+			echo('<td><a href="removeCart.php?id=' . substr($id, 1) . '">&#10060;</a></td>');
+			echo('<td><input type="submit" value="Update"></form></td>');
+			echo("</tr></tr>");
+			$total = $total +$prod['quantity']*$price;
+		}
+		echo("<tr><td colspan=\"4\" align=\"right\"><b>Order Total</b></td><td align=\"right\">$" . number_format($total,2) ."</td></tr>");
+		echo("</table></div>");
+
+		echo("</div><div align='center'><a href=\"checkout.php\"><button class='btn btn-primary mb-1'>Check Out</button></a></button>");
+	} else{
+		echo("<div align='center'><H1>Your shopping cart is empty!</H1>");
+	}
+}
 ?>
 
 <!DOCTYPE html>
