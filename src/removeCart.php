c743f8fe28c0c0a7e6a453433b900efacc37989c
Nathan Nesbitt <nathan@nesbitt.ca>
2019-12-06 15:55:28 -0800

    Fixed missing Object Call

--- src/removeCart.php
+++ src/removeCart.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once 'objects/ShoppingCart.php';
-removeItemFromCart();
+ShoppingCart::removeItemFromCart();
 ?>
--- src/updateQuantity.php
+++ src/updateQuantity.php
@@ -1,6 +1,6 @@
 <?php
 
     require_once 'objects/ShoppingCart.php';
-    changeQuantity();
+    ShoppingCart::changeQuantity();
     
 ?>
